741e150162f0333d94085a03bc5160d99403269b
saifultech <saifultech.official@gmail.com>
2022-12-01 09:52:08 +0800

    update

--- application/views/dashboard/user.php
+++ application/views/dashboard/user.php
@@ -1,7 +1,7 @@
 <?php  if(  $role = $this->session->userdata('role_id')!=3){ ?>
 <div id="back-menu">
-    <a href="<?= base_url('auth') ?>"><button class="button"
-            style="vertical-align:middle"><span><b>KEMBALI</b></span></button></a><br><br><br>
+    <a href="<?= base_url('auth') ?>"><button class="button" style="vertical-align:middle"><span><b>KEMBALI
+                </b></span></button></a><br><br><br>
 </div>
 <?php }?>
 
